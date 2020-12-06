1a9169f598ef653acbd46f71bb9f7ef5e62cee6a
Felipe Hargreaves <fhargreaves00@gmail.com>
2020-12-06 15:51:12 -0300

    Show knapsack result and weights/values, add instructions at each user click, fix a couple of bugs.

--- static/graph.js
+++ static/graph.js
@@ -1,172 +1,172 @@
-let g = [
+const g = [
   {
-    "city1": 'h8',
-    "city2": 'h6'
+    "city1": "h8",
+    "city2": "h6"
   },
   {
-    "city1": 'h4',
-    "city2": 'r10'
+    "city1": "h4",
+    "city2": "r10"
   },
   {
-    "city1": 'h5',
-    "city2": 'h4'
+    "city1": "h5",
+    "city2": "h4"
   },
   {
-    "city1": 'h6',
-    "city2": 'r10'
+    "city1": "h6",
+    "city2": "r10"
   },
   {
-    "city1": 'r10',
-    "city2": 'h1'
+    "city1": "r10",
+    "city2": "h1"
   },
   {
-    "city1": 'h5',
-    "city2": 'h1'
+    "city1": "h5",
+    "city2": "h1"
   },
   {
-    "city1": 'h5',
-    "city2": 'h7'
+    "city1": "h5",
+    "city2": "h7"
   },
   {
-    "city1": 'h7',
-    "city2": 'h2'
+    "city1": "h7",
+    "city2": "h2"
   },
   {
-    "city1": 'h7',
-    "city2": 'h3'
+    "city1": "h7",
+    "city2": "h3"
   },
   {
-    "city1": 'h2',
-    "city2": 'h3'
+    "city1": "h2",
+    "city2": "h3"
   },
   {
-    "city1": 'h2',
-    "city2": 'h1'
+    "city1": "h2",
+    "city2": "h1"
   },
   {
-    "city1": 'h2',
-    "city2": 'r9'
+    "city1": "h2",
+    "city2": "r9"
   },
   {
-    "city1": 'h1',
-    "city2": 'r9'
+    "city1": "h1",
+    "city2": "r9"
   },
   {
-    "city1": 'h6',
-    "city2": 'h5'
+    "city1": "h6",
+    "city2": "h5"
   },
   {
-    "city1": 'h3',
-    "city2": 'r8'
+    "city1": "h3",
+    "city2": "r8"
   },
   {
-    "city1": 'h2',
-    "city2": 'r1'
+    "city1": "h2",
+    "city2": "r1"
   },
   {
     "city1": "r1",
-    "city2": "r3",
+    "city2": "r3"
   },
   {
       "city1": "r1",
-      "city2": "r6",
+      "city2": "r6"
   },
   {
       "city1": "r1",
-      "city2": "r9",
+      "city2": "r9"
   },
   {
       "city1": "r2",
-      "city2": "r6",
+      "city2": "r6"
   },
   {
       "city1": "r2",
-      "city2": "r5",
+      "city2": "r5"
   },
   {
       "city1": "r2",
-      "city2": "r3",
+      "city2": "r3"
   },
   {
       "city1": "r3",
-      "city2": "r8",
+      "city2": "r8"
   },
   {
       "city1": "r3",
-      "city2": "r6",
+      "city2": "r6"
   },
 
   {
       "city1": "r5",
-      "city2": "r6",
+      "city2": "r6"
   },
   {
       "city1": "r6",
-      "city2": "r9",
+      "city2": "r9"
   },
   {
       "city1": "r6",
-      "city2": "r7",
+      "city2": "r7"
   },
   {
       "city1": "r7",
-      "city2": "r9",
+      "city2": "r9"
   },
   {
     "city1": "r13",
-    "city2": "h6",
+    "city2": "h6"
   },
   {
     "city1": "r13",
-    "city2": "h8",
+    "city2": "h8"
   },
   {
     "city1": "h6",
-    "city2": "r15",
+    "city2": "r15"
   },
   {
     "city1": "h7",
-    "city2": "r15",
+    "city2": "r15"
   },
   {
     "city1": "h2",
-    "city2": "r12",
+    "city2": "r12"
   },
   {
     "city1": "h7",
-    "city2": "r12",
+    "city2": "r12"
   },
   {
     "city1": "h3",
-    "city2": "r12",
+    "city2": "r12"
   },
   {
     "city1": "r12",
-    "city2": "r14",
+    "city2": "r14"
   },
   {
     "city1": "r15",
-    "city2": "r14",
+    "city2": "r14"
   },
   {
     "city1": "r8",
-    "city2": "r15",
+    "city2": "r15"
   },
   {
     "city1": "r8",
-    "city2": "r1",
+    "city2": "r1"
   },
   {
     "city1": "h4",
-    "city2": "r11",
+    "city2": "r11"
   },
   {
     "city1": "r10",
-    "city2": "r11",
+    "city2": "r11"
   },
   {
     "city1": "h7",
-    "city2": "r11",
-  },
+    "city2": "r11"
+  }
   
 ]
---
+++ static/routes.js
@@ -0,0 +1,170 @@
+const routes = {
+    "h8-h6": {
+        "geometry": "xwk_BxsrcH]IY~AqEu@kLwd@]HPr@ONwRkEm@BcEaEmE_BmCWYo@E}BiPuDLk@",
+        "distance": 2137.7
+    },
+    "h4-r10": {
+        "geometry": "hll_BhwrcH]GKh@dAR\\}ApE`BfKfB|@VUnA`@PxCaNzB}Ddi@{[h@Aj@rAtDhOS`BoBx@oAa@]y@DcA|@mA|y@kHrGiAnE}AQU}PjDct@pG",
+        "distance": 5222.1
+    },
+    "h5-h4": {
+        "geometry": "`ti_B|}ocH_AW[dIjIrBq@p@eDs@Sb@ff@zLpC`QrNfk@\\CE}AhZtGn@{C[u@",
+        "distance": 3167.9
+    },
+    "h6-r10": {
+        "geometry": "dii_B|tpcHRaAdOrD^aInp@`Pje@VvIwAbf@yWfCjBnTbx@i@zBsCNm@oCdByAtx@_HbNgDsfAfL",
+        "distance": 7270.9
+    },
+    "r10-h1": {
+        "geometry": "n{n_Bf}qcHmC~@[bCd@hApFdFX~A[Z}EoD_BeDMeD~@_D@cCgAqByBeAxF}DmLke@GeBd@aBdRwKpB@fEnBlDm@pEcH\\{ABeDuAqFRu@dRsMx@GpCdEtBaBf@FnRvYg@`@",
+        "distance": 4634.7
+    },
+    "h5-h1": {
+        "geometry": "`ti_B|}ocH_AW[dIjIrBkF`@l|@|Rbg@_@rf@mX~BWrBfBpH|Wd@u@{GsXHiD`RmLhCEfEnBlDm@nF_KqAkLfQaNjB[pCdE|CyAnRvYg@`@",
+        "distance": 7035.5
+    },
+    "h5-h7": {
+        "geometry": "`ti_B|}ocH_AWN{@hCuEbAsEf@_@F[iMgCm@gApK_g@rA]fBd@b@h@b@Jp@OzFpApCgMr@P",
+        "distance": 2039.5
+    },
+    "h7-h2": {
+        "geometry": "`oj_B|smcHs@QpAeGtAZJ[aQeEk@s@lJ_c@nM_Z`I{^`PsYp@CbU~LxJdHpUtSt@H~AuAtGrGvP~RHTaC`CtVr_@sDfC",
+        "distance": 5953.7
+    },
+    "h7-h3": {
+        "geometry": "`oj_B|smcHs@QpAeGtAZJ[aQeEk@s@lJ_c@nM_Z`I{^zOoYv@GbU~LxJdHpUtSt@HxAwAbBpArA_C|A|A",
+        "distance": 4566.1
+    },
+    "h2-h3": {
+        "geometry": "d|n_BnylcH`IsFhA|AzAgAaGkJ?c@[C}QiX?UUAqJoL_JmJDONU|A|A",
+        "distance": 1887.4
+    },
+    "h2-h1": {
+        "geometry": "d|n_BnylcH`IsFPPnTz\\@`@wBdB~B~DvObVtBaBZ?ZV~QfYg@`@",
+        "distance": 2312.4
+    },
+    "h2-r9": {
+        "geometry": "d|n_BnylcH`IsF|M|SzN_Kvb@bp@XWgCaEzC_CJqAvHoF`@DxLrSIn@{HlFfChE",
+        "distance": 3436
+    },
+    "h1-r9": {
+        "geometry": "pdq_BtjocHf@a@tDxFxAdD\\FvNiKB}@k@_ApCkB^gB~FkEhAUFOgHgKwC~AyErDfChE",
+        "distance": 1798.8
+    },
+    "h6-h5": {
+        "geometry": "dii_B|tpcHRaAdOrDE}DHkBZW|Bd@NKC_@oBa@Mm@DWkIsBRwAFmF~@V",
+        "distance": 1138.9
+    },
+    "h3-r8": {
+        "geometry": "|{m_BttjcH}A}Af@}@uAhAec@s^y@OsA`@eIiEIc@xEaMfC_ChC_GfTqh@|FyV^oEx@W`AXrNtL^wAiAu@v@oBEgA",
+        "distance": 3888.6
+    },
+    "h2-r1": {
+        "geometry": "d|n_BnylcH`IsFhA|AzAgAaGkJpK_IA_AwQcXbDcCRoAlBiBtE{CmK_LY{@lRkRt@`@U^i@GkZgZRiExEaHtAVlA|AaCpC`GxF",
+        "distance": 4163
+    },
+    "r1-r3": {
+        "geometry": "`ho_BfxhcHaGyF`CqCyBkDmDqAaI_HGo@~F_IpAFjAyAhLzJXIqFsF}DmG~@uB~DyEzAg@lXoB~UmJbAF\\dAu@~@_Bo@yZew@s@mE`CsE",
+        "distance": 4545.2
+    },
+    "r1-r6": {
+        "geometry": "`ho_BfxhcHaGyF`CqCyBkDmDqAaI_HGo@~F_IpAFjAyAhLzJXI_EyDiFmHx@oC~B_DzCaBnZeCpt@c[zFoGrDoMxEkDfHwApBlAgAnTpA|KbCxFxD|EzMtGsBnF",
+        "distance": 5908.2
+    },
+    "r1-r9": {
+        "geometry": "`ho_BfxhcHaGyF`CqCaDcEYTVv@_EpF_JnKm@Sn@WdR`QhJ~JEr@i@?Ko@jA_AxAJxHnIfStY`aAd{ABdB{NhJ~AhCGv@}HtFhC`E",
+        "distance": 5647.3
+    },
+    "r2-r6": {
+        "geometry": "|zo_Br`~bH|BzDBbCiQbe@iY|[wJ`ZcK|GmA]RcAhAN|HpJ`e@x_A`i@dtAJ~BoBLJyBdh@cUjEkFpFwObM{D|BzBsA`SpA|K|HvMzMtGsBnF",
+        "distance": 8992.8
+    },
+    "r2-r5": {
+        "geometry": "|zo_Br`~bH|BzDBbCiQbe@iY|[wJ`ZcK|GqAu@`B[|HpJ`e@x_A`i@dtAJ~BoBLJyBri@kVvEkHxBgJ`C{BxNyC~C|@?xBwB[\\mFvGuN|HeGbM{BEr@yCDYlB",
+        "distance": 8980.5
+    },
+    "r2-r3": {
+        "geometry": "|zo_Br`~bH|BzDBbCiQbe@iY|[wJ`ZcK|Go@?a@u@d@q@z@T|HpJtNvWlD~K|O`ZrNp^n@j@ZSIw@kAaG`CsE",
+        "distance": 5085.1
+    },
+    "r3-r8": {
+        "geometry": "j~o_BjkdcHfG{JF}Au@wJe@Ap@~Kk@jBgAvAeCfAm@?_IoBy@e@sBeFoNyWi@?Sd@vKdS~C|GrRlg@|IfTdBpG?b@gChLoEnBuBd@uV|AaB\\gA`@}D~EoA\\cHwCkKcJ\\m@iAu@v@oBSo@LW",
+        "distance": 5702.8
+    },
+    "r3-r6": {
+        "geometry": "j~o_BjkdcHfG{Jm@uMe@Ap@~KsBbEsDfA_IoBy@e@cR__@i@?Sd@vPb\\zf@|oAJ~Bo@b@_AUQw@\\aAdh@cUjEkFhC}JfByCzCcBfHwApAb@j@vAsA`SpA|KbCxFxD|EzMtGsBnF",
+        "distance": 7298.9
+    },
+    "r5-r6": {
+        "geometry": "pxs_BpcdcH@KjDL{@vTkU}@o@i@oAyCVk@E]k@Gw@hBy@dEiCjWDtGf@tDbAtD|AdDxD|E`CxAxIzDsBnF",
+        "distance": 2739.2
+    },
+    "r6-r9": {
+        "geometry": "rps_B~`gcHrBoF`YjM~NbN~EfIrElLvKvd@eAlAo}@pViAWUkA^}@r@WvAl@bo@vnBfB|FChBo@fAo`@rX{M|EkA?wAiAiH{K`@_AVx@}RfNkAZ}Rm[yJrGfChE",
+        "distance": 9006.5
+    },
+    "r6-r7": {
+        "geometry": "rps_B~`gcHrBoF`YjMxHnG`GdH~FxKfIlZz@SoBeIT[xSgFnVndAmNtDq@oA",
+        "distance": 4119.4
+    },
+    "r7-r9": {
+        "geometry": "xlv_BpwjcHp@nAlNuDrK`f@~EhIhIfHoMhQcA{@k@r@pHjGfF|JzSn_BQ`IoCTcFoBqMZge@}H_q@_CmJqCcJcIgo@}aAw@oCxA_Bj@j@aBm@l@{@F|@oUjOvA`E}HtFhC`E",
+        "distance": 9687.7
+    },
+    "r13-h6": {
+        "geometry": "`rj_B|_rcHnCj@yC_LqBeEoCsDwBiBwAo@uBo@mCWYo@E}BiPuDLk@",
+        "distance": 1311.3
+    },
+    "r13-h8": {
+        "geometry": "`rj_B|_rcHnCj@Pr@bRhEjD~MRBHGYqARKtDh@X_B\\H",
+        "distance": 995.8
+    },
+    "h6-r15": {
+        "geometry": "dii_B|tpcHRaAdOrDBiHZWlCXC_@qQuD{@qAiDwN~c@stB~Mg[pLkl@w@k@qQcEcArDs@Pd@a\\gBX}G_ATiIYB[dE{CK",
+        "distance": 6443.3
+    },
+    "h7-r15": {
+        "geometry": "`oj_B|smcHs@QpAeGtAZJ[aQeEk@s@lJ_c@jNg\\tKcj@w@k@qQcEcArDs@Pd@a\\gBX}G_ATiIYB[dE{CK",
+        "distance": 4266.4
+    },
+    "h2-r12": {
+        "geometry": "d|n_BnylcH`IsFjMbSqLvIyBRQr@zAtDw@dJr@tFbBnDvDjEp@rCrCnBj@`CCdD]zAqFxH}DNyDyBwBGgQrIaBY}AiByg@gfAcGqK}BmAym@iN?j@nFlBaBlH",
+        "distance": 6173.7
+    },
+    "h7-r12": {
+        "geometry": "`oj_B|smcHs@QpAeGtAZNOCK{A]T_AdH|A`@BDGbFqUjCl@b@d@j@PaBlH",
+        "distance": 1248.4
+    },
+    "h3-r12": {
+        "geometry": "|{m_BttjcH}A}Af@}@uAhAg[mXyGuEqB`@ePsHm@T[xAtC|Ai@z@}@DaE}ByBh@mMpW}AxLn@rGInDkAhCkCvAcC~KrD~FnD~CpEzAnNjAg@xC`GrAcBbI_ZyG?j@nFlBaBlH",
+        "distance": 5294.6
+    },
+    "r12-r14": {
+        "geometry": "ffk_BbamcH`BmHxM|Ch@MJe@cq@aP|ByJ|LwX`I}^",
+        "distance": 2731.5
+    },
+    "r15-r14": {
+        "geometry": "d~i_B`shcHcAC?VhDJ@Yr@D[xCrIv@p@d@`@fAM|E}ArEx@fCHhAGhJaFzT`UhFtEsT",
+        "distance": 2304.8
+    },
+    "r8-r15": {
+        "geometry": "ven_BfmfcHDfAw@nB{AcAUXqMaLyG}CaJyBcE^kLcB}VgG{Lhb@mJpXeG`UkIyA_GYI`@rERcA|O{CK",
+        "distance": 4128.5
+    },
+    "r8-r1": {
+        "geometry": "ven_BfmfcHMVRn@w@nBbI|Fm@v@uDaDUDCRxRrPgAjAG`@X|@_G~Hg@@?NpJ|HpAb@p@bBtAVlA|AaCpC`GxF",
+        "distance": 2162.6
+    },
+    "h4-r11": {
+        "geometry": "hll_BhwrcH]GKh@dAR\\}ApE`BfKfB|@VUnA`@PxCaNvAuCdC_C~G}Df@bA}FbD",
+        "distance": 1459
+    },
+    "r10-r11": {
+        "geometry": "n{n_Bf}qcHkALaAp@Wl@CtAd@hApFdFTd@Bx@[Z]G_EgDs@eAk@_BMeD~@_D@cCMq@y@_AyBeApCyA]e@o`@nUQ\\NTlDsBf@bA}FbD",
+        "distance": 2326.9
+    },
+    "h7-r11": {
+        "geometry": "`oj_B|smcHs@QpAeGbq@xOhCpA`BnB|i@rjAhFbItF|BbLrS}\\bSAr@lDsBf@bA}FbD",
+        "distance": 4590.8
+    }
+}
